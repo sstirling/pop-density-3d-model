c1840b15e2057ac55bbe6310419b106e85f2d745
Stephen Stirling <sstirli1@lmgnj-m0266.advancelocal.net>
2019-04-16 16:36:50 -0400

    second attempt

--- extrude.js
+++ extrude.js
@@ -6,7 +6,7 @@ shp2stl.shp2stl(file,
         width: 1000, //in STL arbitrary units, but typically 3D printers use mm
         height: 1000,
         extraBaseHeight: 0,
-        extrudeBy: "popden2017",
+        extrudeBy: "POPDEN2012,C,6",
         simplification: .8,
         binary: true,
         cutoutHoles: false,
